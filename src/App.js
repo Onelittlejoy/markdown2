c37f224a682eb38bdd7594d084b1b90ebfadcf07
Lihua <lihua@albertoolivaress-MacBook-Pro.local>
2024-03-28 20:12:37 +0100

    commit message here

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import Badge from "react-bootstrap/Badge";
-import { marked } from 'marked';
+import { marked } from "marked"; // Importing directly without default export
 
-export default class App extends React.Component {
+export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      markdown: "",
-    }
+      markdown: `# Welcome to my React Markdown Previewer!
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == ' \`\`\` ' && lastLine == ' \`\`\` ') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`,
+    };
   }
 
   updateMarkdown(markdown) {
-    this.setState({ markdown })
+    this.setState({ markdown });
   }
 
-  render(
-  ) {
+  render() {
     var inputStyle = {
       width: "400px",
       height: "50vh",
@@ -35,7 +75,6 @@ export default class App extends React.Component {
 
     return (
       <div className="App">
-
         <div className="container">
           <div className="row mt-4">
             <div className="col text-center">
@@ -49,22 +88,38 @@ export default class App extends React.Component {
           <div className="row mt-4">
             <div className="col-md-6">
               <div className="col text-center">
-                <h4><Badge className="text-align-center" variant="secondary">
-                  Markdown Input</Badge></h4>
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Markdown Input
+                  </Badge>
+                </h4>
               </div>
               <div className="mark-input" style={inputStyle}>
-                <textarea className="input" style={inputStyle} value={this.state.markdown}
-                  onChange={(e) => { this.updateMarkdown(e.target.value) }}>{console.log(this.state.markdown)}</textarea>
+                <textarea
+                  id="editor"
+                  className="input"
+                  style={inputStyle}
+                  value={this.state.markdown}
+                  onChange={(e) => {
+                    this.updateMarkdown(e.target.value);
+                  }}
+                ></textarea>
               </div>
             </div>
             <div className="col-md-6">
               <div className="col text-center">
-                <h4><Badge className="text-align-center" variant="secondary">
-                  Markdown Output</Badge></h4>
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Markdown Output
+                  </Badge>
+                </h4>
               </div>
               <div
                 style={outputStyle}
-                dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+                dangerouslySetInnerHTML={{
+                  __html: marked(this.state.markdown),
+                }}
+                id="preview"
               ></div>
             </div>
           </div>
